891c442507a25b277875a5372f22c96db5e16a05
Maya Morales <mayainomorales@gmail.com>
2022-04-21 22:02:27 -0700

    added string template literal and functions

---
+++ generate.md
@@ -0,0 +1,37 @@
+# Frankie Ocean
+
+        
+*[Description](#Description)
+        
+*[Installation](#Installation)
+        
+*[Usage](#Usage)
+        
+*[Credits](#Credits)
+
+        
+## Description
+        
+The best artist to ever exist.
+        
+## Installation
+        
+Download Spotify
+        
+## Usage
+        
+Listen to all albums
+        
+## Credits
+        
+frankie.ocean
+        
+frankieOcean
+        
+## License
+        
+Licensed under the MIT License
+        
+## Badges
+        
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- index.js
+++ index.js
@@ -43,17 +43,36 @@ inquirer
 
     function generateReadMe(inputObj){
         console.log(inputObj);
-        const readmeStr = ``
+        const readmeStr = `# ${inputObj.title}
+
+        \n*[Description](#Description)
+        \n*[Installation](#Installation)
+        \n*[Usage](#Usage)
+        \n*[Credits](#Credits)
+
+        \n## Description
+        \n${inputObj.description}
+        \n## Installation
+        \n${inputObj.installation}
+        \n## Usage
+        \n${inputObj.usage}
+        \n## Credits
+        \n${inputObj.github}
+        \n${inputObj.linkedin}
+        \n## License
+        \nLicensed under the MIT License
+        \n## Badges
+        \n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
 
         writeReadMe(readmeStr);
     
         function writeReadMe(readmeStr){
             console.log(readmeStr);
-            fs.writeFile(generate.md, readmeStr, function(err){
+            fs.writeFile('generate.md', readmeStr, function(err){
                 if(err){
                     throw err;
                 }else{
                     console.log('Yay!');
                 }
-            }
-        }
+            })
+        }}
